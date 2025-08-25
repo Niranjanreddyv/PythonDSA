02ea43b34b35b7dd96c81f42cb7648e19b46b549
Niranjan Reddy <niranjanreddyvakareddy999@gmail.com>
2025-08-25 22:59:21 +0530

    operators basics code add

---
+++ basics/Operators/ArithmeticOperators.py
@@ -0,0 +1,7 @@
+print(4+5)
+print(5-4)
+print(5*3)
+print(5/3) # division quotiant -> float
+print(5//3) # loor division int
+print(5%3) # remainder
+print(5**3) # power
---
+++ basics/Operators/AssignmentOperators.py
@@ -0,0 +1,12 @@
+a = 3
+b = 2
+a += 4
+print(a)
+a -= 2
+print(a)
+a *= 2
+print(a)
+a //= 2
+print(a)
+a %= 2
+print(a)
